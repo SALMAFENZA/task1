c1cfed3d5c06dd5dee31eadf9852e78d511b0108
salmafenza <salmafenxza@gmail.com>
2022-10-03 22:05:24 -0300

    task 1 ex1-10

---
+++ repasojs11.js
@@ -0,0 +1,44 @@
+let miNombre="Tobias"
+let miApellido="Perez"
+let miEdad="22"
+let miMascota="Moon"
+let edadMascota="2"
+console.log(miNombre)
+console.log(miApellido)
+console.log(miEdad)
+console.log(miMascota)
+console.log(edadMascota)
+
+let nombreCompleto = miNombre +"."+ miApellido
+
+let textoPresentación = nombreCompleto + "bla bla bla bla"
++"es un tipo genial de" +miEdad+" años. Tiene una mascota llamada "+miMascota+"que tiene"+edadMascota+"."
+
+// punto 9
+
+miEdad = parseInt ( prompt ("Qué edad tenés?"))
+edadMascota = parseInt ( prompt ("qué edad tenés?"))
+
+let sumaEdades = miEdad + edadMascota;
+console.log(sumaEdades)
+let restaEdades = miEdad - edadMascota;
+console.log(restaEdades)
+let productoEdades = miEdad * edadMascota;
+console.log(productoEdades)
+let divisionEdades = miEdad / edadMascota;
+console.log(divisionEdades)
+
+//punto10
+
+miNombre = prompt ("Como te llamas")
+miApellido = prompt ("Como es tu apellido")
+miEdad = prompt ("Que edad tenés?")
+miMascota = prompt ("Como se llama tu gato?")
+edadMascota = prompt ("Cuantos años tiene?")
+console.log  (miNombre)
+console.log (miApellido)
+console.log (miEdad)
+console.log (miMascota)
+console.log (edadMascota)
+
+
